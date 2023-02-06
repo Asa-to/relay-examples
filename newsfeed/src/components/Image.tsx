a348c80cf1c8b734e2161f9e73e931a101e41029
Asa-to <sethuna03@gmail.com>
2023-02-06 23:03:24 +0900

    Imageコンポーネントがurlをフラグメントで指定して自分で取るように変更

--- newsfeed/src/components/Image.tsx
+++ newsfeed/src/components/Image.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
+import { useFragment } from "react-relay";
+import { graphql } from "relay-runtime";
+import type { ImageFragment$key } from "./__generated__/ImageFragment.graphql";
 
 type Props = {
-  image: {
-    url: string;
-  };
+  image: ImageFragment$key;
   width?: number;
   height?: number;
   className?: string;
 };
+
+const ImageFragment = graphql`
+  fragment ImageFragment on Image {
+    url
+    altText
+  }
+`;
 
 export default function Image({
   image,
@@ -15,13 +23,15 @@ export default function Image({
   height,
   className,
 }: Props): React.ReactElement {
+  const data = useFragment(ImageFragment, image);
   if (image == null) {
     return null;
   }
   return (
     <img
-      key={image.url}
-      src={image.url}
+      key={data.url}
+      src={data.url}
+      alt={data.altText}
       width={width}
       height={height}
       className={className}
